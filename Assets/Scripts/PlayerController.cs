94032c0d94c8c4adc8c720c6795f2d70479ffa26
Ewald Hew <ewaldhew@gmail.com>
2021-01-30 03:02:23 +0800

    gameplay: Implement basic player movement control

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController), typeof(PlayerInput))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField]
+    GameObject playerCamera;
+    [SerializeField]
+    PlayerInput playerInput;
+    [SerializeField]
+    CharacterController baseController;
+
+    [Header("Movement")]
+    [SerializeField, Range(0f, 50f),
+        Tooltip("Base movement speed")]
+    float moveSpeed = 10f;
+
+    [SerializeField, Range(0f, 10f),
+        Tooltip("Scale of acceleration in air")]
+    float airControlFactor = 0.1f;
+
+    [SerializeField, Range(0f, 10f)]
+    float jumpSpeed = 10f;
+    [SerializeField]
+    float gravityAcceleration = 30f;
+
+    [SerializeField, Range(0f, 100f),
+        Tooltip("Maximum acceleration. Higher values gives more responsive and jerky movement")]
+    float maxAcceleration = 40f;
+
+    [SerializeField, Range(0f, 2000f),
+        Tooltip("Rotation speed for moving the camera")]
+    float rotationSpeed = 200f;
+
+    [SerializeField,
+        Tooltip("Movement bounds")]
+    Rect movementBounds;
+
+    [SerializeField,
+        Tooltip("Ground check physics layer mask")]
+    LayerMask groundCheckLayerMask;
+
+    private Vector3 ColliderBottom
+    {
+        get
+        {
+            return transform.position - (transform.up * baseController.radius);
+        }
+    }
+    private Vector3 ColliderTop
+    {
+        get
+        {
+            return transform.position + (transform.up * (baseController.height - baseController.radius));
+        }
+    }
+
+    private Vector3 velocity;
+
+    private float jumpTime = 0;
+    private bool shouldJump = false;
+    private bool isOnGround;
+    private Vector3 groundNormal;
+
+    private const float GROUND_CHECK_DISTANCE = 0.05f;
+    private const float MIN_JUMP_TIME = 0.5f; // NOTE: arbitrary, can be found by solving a quadratic though
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // update ground state
+        {
+            isOnGround = false;
+            groundNormal = Vector3.up;
+
+            float maxGroundingDistance = baseController.skinWidth + GROUND_CHECK_DISTANCE;
+
+            if (Time.time >= jumpTime + MIN_JUMP_TIME &&
+                Physics.CapsuleCast(ColliderBottom, ColliderTop, baseController.radius, Vector3.down, out RaycastHit hit, maxGroundingDistance, groundCheckLayerMask, QueryTriggerInteraction.Ignore)) {
+                // for use in projecting move direction
+                groundNormal = hit.normal;
+
+                bool isGroundFacingUp = Vector3.Dot(hit.normal, transform.up) > 0f;
+                bool isSlopeBelowLimit = Vector3.Angle(hit.normal, transform.up) <= baseController.slopeLimit;
+                isOnGround = isGroundFacingUp && isSlopeBelowLimit;
+
+                // snap to ground
+                if (isOnGround && hit.distance > baseController.skinWidth) {
+                    baseController.Move(hit.distance * Vector3.down);
+                }
+            }
+
+            if (isOnGround) {
+                velocity.y = 0;
+            }
+        }
+
+        // handle input
+        {
+            // camera control
+            {
+                float rx = playerInput.GetLookInputsHorizontal() * rotationSpeed;
+                float ry = playerInput.GetLookInputsVertical() * rotationSpeed;
+                float ryClamped = Mathf.Clamp(playerCamera.transform.localEulerAngles.x - ry, 20, 60);
+
+                transform.Rotate(new Vector3(0f, rx, 0f), Space.Self);
+                playerCamera.transform.localEulerAngles = new Vector3(ryClamped, 0f, 0f);
+            }
+
+            // movement
+            {
+                Vector3 inputVelocity = transform.TransformVector(playerInput.GetMoveInput()) * moveSpeed;
+                Vector3 tangent = Vector3.Cross(inputVelocity.normalized, transform.up);
+                Vector3 targetVelocity = Vector3.Cross(groundNormal, tangent).normalized * inputVelocity.magnitude;
+
+                float acceleration = isOnGround ? maxAcceleration : maxAcceleration * airControlFactor;
+                float maxSpeedChange = acceleration * Time.deltaTime;
+                float vx = Mathf.MoveTowards(velocity.x, targetVelocity.x, maxSpeedChange);
+                float vy = Mathf.MoveTowards(velocity.y, targetVelocity.y, maxSpeedChange);
+                float vz = Mathf.MoveTowards(velocity.z, targetVelocity.z, maxSpeedChange);
+                velocity = new Vector3(vx, isOnGround ? vy : velocity.y, vz);
+
+                // add jump
+                shouldJump |= isOnGround && playerInput.GetJumpInputDown();
+                if (isOnGround && shouldJump) {
+                    velocity.y = jumpSpeed;
+
+                    isOnGround = false;
+                    groundNormal = Vector3.up;
+                    shouldJump = false;
+                    jumpTime = Time.time;
+                }
+
+                // apply gravity
+                if (!isOnGround) {
+                    velocity += Vector3.down * gravityAcceleration * Time.deltaTime;
+                }
+
+                // collide with other geometry
+                Vector3 postVelocity = velocity;
+                if (Physics.CapsuleCast(ColliderBottom, ColliderTop, baseController.radius, velocity.normalized, out RaycastHit hit, velocity.magnitude * Time.deltaTime, 0, QueryTriggerInteraction.Ignore)) {
+                    postVelocity = Vector3.ProjectOnPlane(velocity, hit.normal);
+                }
+
+                baseController.Move(velocity * Time.deltaTime);
+                velocity = postVelocity;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [Tooltip("Sensitivity multiplier for moving the camera around")]
+    public float lookSensitivity = 1f;
+    [Tooltip("Additional sensitivity multiplier for WebGL")]
+    public float webglLookSensitivityMultiplier = 0.25f;
+    [Tooltip("Limit to consider an input when using a trigger on a controller")]
+    public float triggerAxisThreshold = 0.4f;
+    [Tooltip("Used to flip the vertical input axis")]
+    public bool invertYAxis = false;
+    [Tooltip("Used to flip the horizontal input axis")]
+    public bool invertXAxis = false;
+
+    bool m_FireInputWasHeld;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void LateUpdate()
+    {
+        m_FireInputWasHeld = GetFireInputHeld();
+    }
+
+    public bool IsInputDisabled()
+    {
+        return Cursor.lockState != CursorLockMode.Locked;
+    }
+
+    public Vector3 GetMoveInput()
+    {
+        if (IsInputDisabled()) {
+            return Vector3.zero;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxisRaw(AppConstants.k_AxisNameHorizontal), 0f, Input.GetAxisRaw(AppConstants.k_AxisNameVertical));
+        move = Vector3.ClampMagnitude(move, 1f);
+
+        return move;
+    }
+
+    public float GetLookInputsHorizontal()
+    {
+        return GetMouseOrStickLookAxis(AppConstants.k_MouseAxisNameHorizontal, AppConstants.k_AxisNameJoystickLookHorizontal);
+    }
+
+    public float GetLookInputsVertical()
+    {
+        return GetMouseOrStickLookAxis(AppConstants.k_MouseAxisNameVertical, AppConstants.k_AxisNameJoystickLookVertical);
+    }
+
+    public bool GetJumpInputDown()
+    {
+        if (IsInputDisabled()) {
+            return false;
+        }
+
+        return Input.GetButtonDown(AppConstants.k_ButtonNameJump);
+    }
+
+    public bool GetJumpInputHeld()
+    {
+        if (IsInputDisabled()) {
+            return false;
+        }
+
+        return Input.GetButton(AppConstants.k_ButtonNameJump);
+    }
+
+    public bool GetFireInputDown()
+    {
+        return GetFireInputHeld() && !m_FireInputWasHeld;
+    }
+
+    public bool GetFireInputReleased()
+    {
+        return !GetFireInputHeld() && m_FireInputWasHeld;
+    }
+
+    public bool GetFireInputHeld()
+    {
+        if (IsInputDisabled()) {
+            return false;
+        }
+
+        bool isGamepad = Input.GetAxis(AppConstants.k_ButtonNameGamepadFire) != 0f;
+        if (isGamepad) {
+            return Input.GetAxis(AppConstants.k_ButtonNameGamepadFire) >= triggerAxisThreshold;
+        } else {
+            return Input.GetButton(AppConstants.k_ButtonNameFire);
+        }
+    }
+
+    public bool GetAimInputHeld()
+    {
+        if (IsInputDisabled()) {
+            return false;
+        }
+
+        bool isGamepad = Input.GetAxis(AppConstants.k_ButtonNameGamepadAim) != 0f;
+        bool i = isGamepad ? (Input.GetAxis(AppConstants.k_ButtonNameGamepadAim) > 0f) : Input.GetButton(AppConstants.k_ButtonNameAim);
+        return i;
+    }
+
+    float GetMouseOrStickLookAxis(string mouseInputName, string stickInputName)
+    {
+        if (IsInputDisabled()) {
+            return 0f;
+        }
+
+        // Check if this look input is coming from the mouse
+        bool isGamepad = Input.GetAxis(stickInputName) != 0f;
+        float i = isGamepad ? Input.GetAxis(stickInputName) : Input.GetAxisRaw(mouseInputName);
+
+        // handle inverting vertical input
+        if (invertYAxis)
+            i *= -1f;
+
+        // apply sensitivity multiplier
+        i *= lookSensitivity;
+
+        if (isGamepad) {
+            // since mouse input is already deltaTime-dependant, only scale input with frame time if it's coming from sticks
+            i *= Time.deltaTime;
+        } else {
+            // reduce mouse input amount to be equivalent to stick movement
+            i *= 0.01f;
+#if UNITY_WEBGL
+            // Mouse tends to be even more sensitive in WebGL due to mouse acceleration, so reduce it even more
+            i *= webglLookSensitivityMultiplier;
+#endif
+        }
+
+        return i;
+    }
+}

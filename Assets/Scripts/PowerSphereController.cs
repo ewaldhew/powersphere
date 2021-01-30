568a80c1af15890ce7378126fa9429f058ee2822
Ewald Hew <ewaldhew@gmail.com>
2021-01-30 19:02:12 +0800

    physics: Add stage boundaries, setup collision with ball objects.

---
+++ Assets/Scripts/PowerSphereController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PowerSphereController : MonoBehaviour
+{
+    [SerializeField]
+    Collider playerCollider;
+    [SerializeField]
+    Collider selfCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other != playerCollider) {
+            return;
+        }
+
+        float distance;
+        Vector3 direction;
+        Physics.ComputePenetration(
+            playerCollider, playerCollider.transform.position, playerCollider.transform.rotation,
+            selfCollider, transform.position, transform.rotation, out direction, out distance);
+
+        selfCollider.attachedRigidbody.AddForce(distance * -direction, ForceMode.Impulse);
+    }
+}
